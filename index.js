db72333391b73117640918a04cdff78654d7d893
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-09 19:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.isLowercase()){
-    return 'I can\'t hear you!' 
-}}
+  if (string === string.toLowerCase()){
+    return 'I can\'t hear you!'; 
+} else if (sting === string.toUpperCase()) {
+  return "YES INDEED!" ;
+} else if (string === 'I love you, Grandma.'){
+  return 'I love you, too.'
+}
+}
